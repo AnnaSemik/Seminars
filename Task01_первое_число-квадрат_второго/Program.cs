14573a0932c5c4131d331b8a791517cae676faa4
Anna <anna-c-91@mail.ru>
2023-10-14 08:00:39 +1200

    Добавлены решения задач из первого семинара

---
+++ Task01_первое_число-квадрат_второго/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа, и проверяет, является ли первое число квадратом второго.
+*/
+
+System.Console.WriteLine("Введите первое число: "); //cw - быстрый вызов Console.Writeline
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второое число: "); //cw - быстрый вызов Console.Writeline
+int userNumber2 = Convert.ToInt32(Console.ReadLine());
+int sq = userNumber2 * userNumber2;
+bool result = sq == userNumber1;
+Console.WriteLine(result);
+
---
+++ Task05_целые_числа_от_-N_до_N/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+Напишите программу, котоая на вход принимает одно число (N), а на выходе показывает все целые числа от -N до N.
+*/
+System.Console.WriteLine("Введите число: "); //cw - быстрый вызов Console.Writeline
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i=-N; i<=N; i++)
+    Console.WriteLine(i);

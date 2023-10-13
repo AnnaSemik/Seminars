07f83363d472d17cbc44766451b7208f442cac52
Anna <anna-c-91@mail.ru>
2023-10-14 07:52:00 +1200

    Initial commit

--- Task00_квадрат_числа/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿/*
-Напишите программу, которая на вход принимает число и выдает его крадрат (число умноженное само на себя).
-*/
-
-System.Console.WriteLine("Введите число: "); //cw - быстрый вызов Console.Writeline
-int userNumber = Convert.ToInt32(Console.ReadLine());
-int result = userNumber * userNumber;
-Console.WriteLine(result);
-Console.WriteLine(userNumber*userNumber);

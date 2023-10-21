d62df0b3d762ac94f3d6801f44b6a34a97d6dae5
Anna <anna-c-91@mail.ru>
2023-10-21 21:21:54 +1200

    Добавлены решения задач 25, 27, 29. Дополнен список задач

---
+++ Task25_натуральная_степень_числа/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+*/
+
+System.Console.WriteLine("Введите число А: "); //cw - быстрый вызов Console.Writeline
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число В: "); //cw - быстрый вызов Console.Writeline
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int thirdNumber = 1;
+if (secondNumber < 1)
+{
+    Console.Write("Число B должно быть натуральным");
+}
+else
+{
+    while (secondNumber != 0)
+    {
+        thirdNumber = thirdNumber * firstNumber;
+        secondNumber = secondNumber - 1;
+    }
+    Console.Write ($"Число А в натуральной степени B равно {thirdNumber}");
+}
---
+++ Task27_сумма_цифр_числа/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int len = Length(number);
+int Length(int number)
+{
+    int index = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        index++;
+    }
+    return index;
+}
+
+int Sum(int number, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+Console.Write($"Сумма цифр в числе { number } равна: { Sum(number, len)}");

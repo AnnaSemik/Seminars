de647d101fe2716ed5afc194b555a5b0312aa4c6
Anna <anna-c-91@mail.ru>
2023-11-07 14:18:50 +1200

    Добавлены решения задач 46б 47, 50, 52

---
+++ Task46_двухмерный_массив_случайных_чисел/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+Задайте двумерный массив размером m × n, заполненный случайными целыми числами.
+*/
+
+// int TakeEnteredNumber(string massage)
+// {
+//     System.Console.WriteLine(massage);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
+
+// int[,] GetRandom2DArray(int start, int end, int row, int col)
+// {
+//     int[,] array = new int[row, col];
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < col; j++)
+//         {
+//             array[i, j] = new Random().Next(start, end + 1);
+//         }
+//     }
+//     return array;
+// }
+
+// void Print2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             System.Console.Write(array[i, j] + "\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// int userArrayR = TakeEnteredNumber("Введите количество строк: ");
+// int userArrayC = TakeEnteredNumber("Введите количество столбцов: ");
+// int userArrayStart = TakeEnteredNumber("Введите начало диапазона: ");
+// int userArrayEnd = TakeEnteredNumber("Введите конец диапазона: ");
+// int[,] user2DArray = GetRandom2DArray(userArrayStart, userArrayEnd, userArrayR, userArrayC);
+// Print2DArray(user2DArray);
+
+
+void Fill2DArray (int[,] arr)
+{
+    for (int i= 0; i < arr.GetLength(0); i++)
+    {
+        for (int j= 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(-10, 100);
+        } 
+    }
+}
+
+void Print2DArray (int[,] array)
+{
+        for (int i= 0; i < array.GetLength(0); i++)
+    {
+        for (int j= 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] array2D = new int[3, 4];
+Fill2DArray(array2D);
+Print2DArray(array2D);
---
+++ Task47_двумерный_массив_случайных_вещественных_чисел/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+*/ 
+
+// void Fill2DArray (double[,] arr)
+// {
+//     for (int i= 0; i < arr.GetLength(0); i++)
+//     {
+//         for (int j= 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(-10, 100);
+//         } 
+//     }
+// }
+
+// void Print2DArray (double[,] array)
+// {
+//         for (int i= 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j= 0; j < array.GetLength(1); j++)
+//         {
+//             System.Console.Write($"{array[i, j]}\t");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+// double[,] array2D = new double[3, 4];
+// Fill2DArray(array2D);
+// Print2DArray(array2D);
+
+int TakeEnteredNumber(string massage)
+{
+    System.Console.WriteLine(massage);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+double[,] GetRandom2DArray(int start, int end, int row, int col)
+{
+    double[,] array = new double[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = new Random().NextDouble()*(end - start) + start;
+        }
+    }
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(Math.Round(array[i, j], 2) + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int userArrayR = TakeEnteredNumber("Введите количество строк: ");
+int userArrayC = TakeEnteredNumber("Введите количество столбцов: ");
+int userArrayStart = TakeEnteredNumber("Введите начало диапазона: ");
+int userArrayEnd = TakeEnteredNumber("Введите конец диапазона: ");
+double[,] user2DArray = GetRandom2DArray(userArrayStart, userArrayEnd, userArrayR, userArrayC);
+Print2DArray(user2DArray);
